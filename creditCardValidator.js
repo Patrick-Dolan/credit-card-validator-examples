a972b49a6b76684b48d9ff4eb0fb61d56571be8a
Patrick Dolan <dolanp1992@gmail.com>
2023-05-16 10:42:30 -0700

    documentation: update code with comments to make it clear and easy to read     update: add luhnsAlgorithmFundamentals function

--- creditCardValidator.js
+++ creditCardValidator.js
@@ -1,3 +1,6 @@
+// Algorithm Notes
+//=================
+
 // Double every other digit
 // Add digits of double digit numbers ex. 16 becomes 1 + 6 become 7 
 // Add all numbers together
@@ -7,6 +10,56 @@
 // Test Numbers
 // Valid: 4102080860435620
 // Invalid: 4102080880435620
+
+//Business Logic
+//==============================================================================
+
+
+// ================================================
+//               Fundamentals Solution
+// ================================================
+
+function luhnsAlgorithmFundamentals(cardNumberString) {
+  // Split card number string in an array to loop over
+  let cardNumArr = cardNumberString.split("");
+
+  // Double every other digit and add together those over 9
+  let processedCardNumbers = [];
+  for (let i = 0; i <= cardNumArr.length - 1; i++) {
+    // Use modulo to seperate logic based on even and odd indexes 
+    if (i % 2 !== 0) {
+      const num = cardNumArr[i] * 2;
+      // Check if number is double digit or not and add digits together if it is
+      if (num.toString().length > 1) {
+        let numString = num.toString();
+        let numArr = numString.split("");
+        processedCardNumbers.push(parseInt(numArr[0]) + parseInt(numArr[1]));
+      } else {
+        processedCardNumbers.push(num);
+      }
+    } else {
+      processedCardNumbers.push(cardNumArr[i]);
+    }
+  }
+
+  // Add all processed card numbers together
+  let summedCardNumbers = 0;
+  for(let i = 0; i <= processedCardNumbers.length - 1; i++) {
+    summedCardNumbers += parseInt(processedCardNumbers[i]);
+  }
+
+  // Return result based on summedCardNumbers
+  let summedCardNumbersString = summedCardNumbers.toString()
+  if (summedCardNumbersString[summedCardNumbersString.length - 1] === "0") {
+    return "This card number is valid.";
+  } else {
+    return "This card number is not valid.";
+  }
+}
+
+// ================================================
+//                Advanced Solution
+// ================================================
 
 const addSequentialDigitsAdvanced = (num) => {
 	return num.toString().split("").map(e => parseInt(e)).reduce((sum, num) => sum + num);
@@ -35,9 +88,12 @@ const luhnsAlgorithmAdvanced = (cardNumberString) => {
 }
 
 // Valid result expected
-// console.log(luhnsAlgorithmAdvanced("4102080860435620"));
+console.log(luhnsAlgorithmFundamentals("4102080860435620"));
 // Invalid result expected
-// console.log(luhnsAlgorithmAdvanced("4102080880435620"));
+console.log(luhnsAlgorithmFundamentals("4102080880435620"));
 
-const cardNumber = window.prompt("Enter a credit card number: ");
-window.alert(luhnsAlgorithmAdvanced(cardNumber));
+
+// UI Logic
+//==============================================================================
+// const cardNumber = window.prompt("Enter a credit card number: ");
+// window.alert(luhnsAlgorithmAdvanced(cardNumber));
